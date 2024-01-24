11ce92711c21c8d13aa6fc2e49af8c808e0cb416
Lor <500154412@fvtc.edu>
2024-01-23 18:49:27 -0600

    Done with query selection

---
+++ site.js
@@ -0,0 +1,25 @@
+
+const h1 = document.querySelector('h1')
+h1.textContent= "Alex Webpage"
+
+const copyright = document.querySelector('footer > p')
+copyright.textContent = "BLA"
+
+const link = document.querySelector('.active')
+link.style.color = "#ff0000"
+link.style.textDecoration = "underline"
+
+const contactLink = document.querySelector('#contact')
+contactLink.style.fontWeight = "bold"
+contactLink.style.fontFamily = "Cambria"
+contactLink.style.color = "blue"
+
+const contactAlt = document.getElementById('contact')
+
+const links = document.querySelectorAll('nav a')
+//links.forEach(link => link.style.display = 'none')
+
+
+
+
+//alert("Hello")
